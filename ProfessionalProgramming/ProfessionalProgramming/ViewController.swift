a96cfb27c09476737015ae2cc535a2cfe4914fde
Yevgeny Ly <globchenko16@gmail.com>
2024-03-25 14:51:54 +0300

    - app init

---
+++ ProfessionalProgramming/ProfessionalProgramming/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ProfessionalProgramming
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
